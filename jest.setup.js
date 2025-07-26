8a25b5368894051db9c53e7935c31d877f8a01a0
badasswp <badasswpdev@gmail.com>
2025-07-26 09:30:54 +0100

    chore: mock WP components at init

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect.js';
 import '@wordpress/jest-preset-default/scripts/setup-globals';
+
+jest.mock( '@wordpress/components', () => {
+	const original = jest.requireActual( '@wordpress/components' );
+	return {
+		...original,
+		Fill: ( { children } ) => <div>{ children }</div>,
+	};
+} );
