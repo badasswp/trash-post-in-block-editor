9287be8424ac86b2f8b6a6d0cb3fc82b251a0bfd
badasswp <badasswpdev@gmail.com>
2025-07-26 09:51:03 +0100

    test: load import from jest.setup

--- tests/Trash.test.tsx
+++ tests/Trash.test.tsx
@@ -1,5 +1,4 @@
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 import TrashPostInBlockEditor from '../src/index';
 import { trashPost } from '../src/utils';
