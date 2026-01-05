abe34fa0ebe3decfb50a84dceb31ee479cbf534b
badasswp <badasswpdev@gmail.com>
2026-01-05 04:15:44 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "Trash Post in Block Editor"
-wp-env run cli wp option update blogdescription "Delete a Post from within the WP Block Editor."
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/trash-post-in-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e04cb50df4b4deb4e7657d66153b42a3db686a7a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/trash-post-in-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e04cb50df4b4deb4e7657d66153b42a3db686a7a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
