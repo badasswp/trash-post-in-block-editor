cbad5aef9d987a39eda450c1b61019c152e7c48d
badasswp <badasswpdev@gmail.com>
2026-04-30 14:22:49 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/trash-post-in-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bafd49e14c2fa0ae7e5fd4c2035293db867b6019',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/trash-post-in-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bafd49e14c2fa0ae7e5fd4c2035293db867b6019',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
