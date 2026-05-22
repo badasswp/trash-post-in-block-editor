db7c26ac8e59215a79115ac3bf9fd7d623ea54d0
badasswp <badasswpdev@gmail.com>
2026-05-22 19:06:45 +0000

    Build from release-1.2.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/trash-post-in-block-editor',
+        'pretty_version' => 'dev-release-1.2.3-built',
+        'version' => 'dev-release-1.2.3-built',
+        'reference' => '756fc0514d120097a1d2e16694e12f7f8251347e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/trash-post-in-block-editor' => array(
+            'pretty_version' => 'dev-release-1.2.3-built',
+            'version' => 'dev-release-1.2.3-built',
+            'reference' => '756fc0514d120097a1d2e16694e12f7f8251347e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
